cfef726d595b23896a97bbe9d03b821d388ffa59
Fizza Usman <52672094+codes-by-kiwi@users.noreply.github.com>
2020-03-30 14:26:19 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+# This prints out "Hello there Frank!"
+name = "Frank"
+print("Hello there %s!" %name)
+
+#The above is also possible where you use %s to indicate that you need to
+#insert a string there and reference the particular string to be 
+#printed using the next "%" symbol
